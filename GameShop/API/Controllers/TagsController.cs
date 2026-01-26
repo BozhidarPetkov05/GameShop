c74a2fc949a51b948011a844fa83b928245cba09
Bozhidar Petkov <xidz01bog@gmail.com>
2026-01-26 23:43:08 +0200

    Tags Controller Added

---
+++ GameShop/API/Controllers/TagsController.cs
@@ -0,0 +1,91 @@
+using System;
+using API.Infrastructure.RequestDTOs.Tags;
+using Common.Entities;
+using Common.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            TagServices service = new TagServices();
+            return Ok(service.GetAll());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            TagServices service = new TagServices();
+            return Ok(service.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] TagRequest model)
+        {
+            if (!User.HasClaim("isAdmin", "True"))
+            {
+                return Forbid();
+            }
+
+            TagServices service = new TagServices();
+            if (service.TagExists(model.Name))
+            {
+                throw new Exception("Tag with this name already exists!");
+            }
+
+            var item = new Tag()
+            {
+                Name = model.Name
+            };
+
+            service.Save(item);
+
+            return Ok(item);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put([FromRoute] int id, [FromBody] TagRequest model)
+        {
+            if (!User.HasClaim("isAdmin", "True"))
+            {
+                return Forbid();
+            }
+
+            TagServices service = new TagServices();
+            if (service.TagExists(model.Name))
+            {
+                throw new Exception("Tag with this name already exists!");
+            }
+
+            Tag forUpdate = service.GetById(id);
+            forUpdate.Name = model.Name;
+            service.Save(forUpdate);
+
+            return Ok(forUpdate);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            if (!User.HasClaim("isAdmin", "True"))
+            {
+                return Forbid();
+            }
+
+            TagServices services = new TagServices();
+            Tag forDelete = services.GetById(id);
+            services.Delete(forDelete);
+            return Ok(forDelete);
+        }
+    }
+}
--- GameShop/Common/Services/TagServices.cs
+++ GameShop/Common/Services/TagServices.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Linq;
 using Common.Entities;
 
 namespace Common.Services;
 
 public class TagServices : BaseService<Tag>
 {
-
+    public bool TagExists(string name)
+    {
+        return Items.Any(t => t.Name == name);
+    }
 }
